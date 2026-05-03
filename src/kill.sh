3a2c7fdfe7644e61c160857de1ca58752629c196
Igor Saric <karabaja4@gmail.com>
2026-05-03 18:51:20 +0200

    a

--- src/kill.sh
+++ src/kill.sh
@@ -32,7 +32,15 @@ fi
 sleep 3
 _out="$(pstree -p)"
 
-_echo "After ${_action}: ${_out}"
+_lc="$(printf '%s' "${_out}" | grep -c -v '^[[:space:]]*$')"
+if [ "${_lc}" -eq 1 ]
+then
+    _echo "After ${_action}: ${_out}"
+else
+    _echo "After ${_action}:"
+    printf '%s\n' "${_out}"
+fi
+
 if printf '%s' "${_out}" | grep -q '^init([0-9][0-9]*)---sh([0-9][0-9]*)---pstree([0-9][0-9]*)$'
 then
     _echo "All processes exited after ${_action}"
