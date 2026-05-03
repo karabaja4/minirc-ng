850e49a73441e3226b9c3243400e94475b10bc63
Igor Saric <karabaja4@gmail.com>
2026-05-03 17:35:56 +0200

    kill script

---
+++ src/kill.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+_kill_file='/tmp/minirc-kill'
+_action='TERM'
+
+if [ "${1}" = 'killcheck' ]
+then
+    if [ -f "${_kill_file}" ]
+    then
+        _action='KILL'
+    else
+        exit 0
+    fi
+fi
+
+_echo() {
+    printf '[%s][%s] %s\n' "${$}" "${0##*/}" "${1}"
+}
+
+_kill() {
+    _echo "Sending ${1} to all"
+    /bin/busybox kill "-${1}" -1
+}
+
+if [ "${_action}" = 'TERM' ]
+then
+    rm -f "${_kill_file}"
+    trap '_echo "Caught TERM"' TERM
+    _kill TERM
+fi
+
+sleep 3
+_out="$(pstree -p)"
+
+_echo "After ${_action}: ${_out}"
+if printf '%s' "${_out}" | grep -q '^init([0-9][0-9]*)---sh([0-9][0-9]*)---pstree([0-9][0-9]*)$'
+then
+    _echo "All processes exited after ${_action}"
+else
+    _echo "Failed to ${_action} all processes"
+    if [ "${_action}" = 'TERM' ]
+    then
+        printf '%s\n' "${_out}" > "${_kill_file}"
+        _kill KILL
+    fi
+fi
